c5f778b8f0e3dde38472cfa692bce1aa0236680c
shcha9552 <ninestra9552@gmail.com>
2025-01-24 11:17:35 +0900

    Initial Commit

---
+++ musinshop/musinshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  musinshopApp.swift
+//  musinshop
+//
+//  Created by cha on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct musinshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ musinshopTests/musinshopTests.swift
@@ -0,0 +1,17 @@
+//
+//  musinshopTests.swift
+//  musinshopTests
+//
+//  Created by cha on 1/24/25.
+//
+
+import Testing
+@testable import musinshop
+
+struct musinshopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
