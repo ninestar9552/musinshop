49a07ed0c1bcf880a72293f9c62ea82483a46ad6
shcha9552 <ninestra9552@gmail.com>
2025-02-07 09:40:22 +0900

    구매하기 버튼 스타일 추가

--- musinshop/Features/Common/CommonButtonStyle.swift
+++ musinshop/Features/Common/CommonButtonStyle.swift
@@ -7,24 +7,27 @@
 
 import SwiftUI
 
-struct RoundedBorderButtonStyle: ButtonStyle {
+struct PriamaryButtonStyle: ButtonStyle {
+    var backgroundColor: Color = .black
     var textColor: Color = .white
-    var borderColor: Color = .white
-    var fontSize: CGFloat = 16
-    var cornerRadius: CGFloat = 25
+    var borderColor: Color = .clear
+    var fontSize: CGFloat = 14
+    var cornerRadius: CGFloat = 4
     var borderWidth: CGFloat = 1
     
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .font(.system(size: fontSize, weight: .semibold))
-            .foregroundColor(textColor)
-            .padding(.vertical, 8) // 위아래 여백
-            .padding(.horizontal, 15) // 좌우 여백
-            .background(Color.clear) // 배경 투명
-            .overlay(
+            .font(.system(size: fontSize, weight: .bold))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(
                 RoundedRectangle(cornerRadius: cornerRadius)
-                    .stroke(borderColor, lineWidth: borderWidth) // 테두리 스타일
+                    .fill(backgroundColor) // 버튼의 배경색
+                    .overlay(
+                        RoundedRectangle(cornerRadius: cornerRadius)
+                            .stroke(borderColor, lineWidth: borderWidth) // 테두리 스타일
+                    )
             )
+            .foregroundColor(textColor)
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0) // 눌렀을 때 크기 효과
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
@@ -38,7 +41,8 @@ struct RoundedBorderButtonStyle: ButtonStyle {
         }) {
             Text("Hello, World!")
         }
-        .buttonStyle(RoundedBorderButtonStyle())
+        .frame(width: 200, height: 44)
+        .buttonStyle(PriamaryButtonStyle())
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .background(Color.gray)
